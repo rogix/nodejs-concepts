89d46bab4568af5285a44db0f4e885c9187176e0
rogix <12768403+rogix@users.noreply.github.com>
2020-10-08 07:53:58 -0300

    Update pkgs and app

--- src/app.js
+++ src/app.js
@@ -75,7 +75,6 @@ app.delete("/repositories/:id", (request, response) => {
 
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
-  const { likes } = request.body;
 
   const repositoryIndex = repositories.findIndex((r) => r.id === id);
 
